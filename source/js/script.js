09336729460a9a81ec8c858cb1f8c02d4f14495a
Михаил <m.juckov2010@gmail.com>
2022-08-29 21:45:10 +0300

    делаю декоративные эелементы

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let burger = document.querySelector(".header__burger");
+let nav = document.querySelector(".header__nav");
+
+burger.addEventListener("click", function () {
+  if (burger.classList.contains("header__burger--closed")) {
+    burger.classList.remove("header__burger--closed");
+    burger.classList.add("header__burger--opened");
+    nav.classList.add("header__nav--opened");
+  } else {
+    burger.classList.add("header__burger--closed");
+    burger.classList.remove("header__burger--opened");
+    nav.classList.remove("header__nav--opened");
+  }
+});
